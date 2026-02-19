e55315695ff33380652c574631529f373c8a4fec
Emman <101398596+blackyaksha@users.noreply.github.com>
2026-02-19 16:29:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.post('/fetch-files', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('Server is running ✅');
 });
+
